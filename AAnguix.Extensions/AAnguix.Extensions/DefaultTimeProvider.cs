9adacfe07266fc1e713abf0f359022fabde430fa
Arturo <arturo.anguix.l@gmail.com>
2018-09-24 20:16:57 +0200

    Time provider implementation.

---
+++ AAnguix.Extensions/AAnguix.Extensions/DefaultTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    public class DefaultTimeProvider : TimeProvider
+    {
+        public override DateTime UtcNow
+        {
+            get { return DateTime.UtcNow; }
+        }
+
+        public override DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+    }
+}
---
+++ AAnguix.Extensions/AAnguix.Extensions/TimeProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Implementation of an static time provider whose implementation can be replaced for testing purposes.
+    /// </summary>
+    public abstract class TimeProvider
+    {
+        private static TimeProvider _current;
+
+        static TimeProvider()
+        {
+            _current = new DefaultTimeProvider();
+        }
+
+        public static TimeProvider Current
+        {
+            get { return _current; }
+            set
+            {
+                _current = value ?? throw new ArgumentNullException(nameof(value));
+            }
+        }
+
+        public abstract DateTime UtcNow { get; }
+
+        public abstract DateTime Now { get; }
+
+        public static void ResetToDefault()
+        {
+            _current = new DefaultTimeProvider();
+        }
+    }
+}
